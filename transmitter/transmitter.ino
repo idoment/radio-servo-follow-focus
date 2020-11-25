c354a167cf221f6742a9b057a08571820944079d
Sandro <sa01100001@gmail.com>
2020-11-25 12:13:55 +0100

    Add files via upload

---
+++ transmitter/transmitter.ino
@@ -0,0 +1,20 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+const uint64_t pipe = 0xE8E8F0F0E1LL; 
+RF24 radio (9, 10);
+int msg[1];
+
+void setup()  
+{
+  radio.begin ();
+  radio.openWritingPipe (pipe);
+}
+
+void loop()  
+{
+  msg[0] =  map (analogRead (0), 0, 1023, 0, 180); 
+  radio.write (msg, 1);
+  delay (1);
+}
